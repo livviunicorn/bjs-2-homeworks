c3acbe24b908f81093ab2f5d47000b4143999827
Katie M <katherine.malysheva.1998@gmail.com>
2026-03-21 19:58:05 +0400

    шестое задание полностью готово

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,60 @@
-﻿
+﻿function parseCount(value) {
+    const parsed = Number.parseFloat(value);
+
+    if (Number.isNaN(parsed)) {
+        throw new Error("Невалидное значение");
+    }
+
+    return parsed;
+}
+
+function validateCount(value) {
+    try {
+        return parseCount(value);
+    } catch (error) {
+        return error;
+    }
+}
+
+class Triangle {
+    constructor(sideOne, sideTwo, sideThree) {
+        this.a = sideOne;
+        this.b = sideTwo;
+        this.c = sideThree;
+
+        if (
+            this.a + this.b <= this.c ||
+            this.a + this.c <= this.b ||
+            this.b + this.c <= this.a
+        ) {
+            throw new Error('Треугольник с такими сторонами не существует');
+        }
+    }
+
+    get perimeter() {
+        return this.a + this.b + this.c;
+    }
+
+    get area() {
+        const p = this.perimeter / 2;
+        const area = Math.sqrt(
+            p * (p - this.a) * (p - this.b) * (p - this.c)
+        );
+        return Number(area.toFixed(3));
+    }
+}
+
+function getTriangle(a, b, c) {
+    try {
+        return new Triangle(a, b, c);
+    } catch (error) {
+        return {
+            get area() {
+                return 'Ошибка! Треугольник не существует';
+            },
+            get perimeter() {
+                return 'Ошибка! Треугольник не существует';
+            }
+        };
+    }
+}
